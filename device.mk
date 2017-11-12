490d60340c1810717f572b20856400550fd57912
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-12 19:29:06 -0200

    X00T: Introduce 'SafailNet'
    Bypass safetynet even if we have permissive selinux
    
    Change-Id: Ib44cdbd15b53af3a1768cb0912f2dbcc56a8d97e

--- device.mk
+++ device.mk
@@ -311,7 +311,8 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qti.dcvs.sh \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.safailnet.rc
     
 # Recovery
 PRODUCT_PACKAGES += \
