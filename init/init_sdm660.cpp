08d8d2e8b66d8740a8ef7d98427c508d3090f3a4
Swamoy14 <shubhasankar14@gmail.com>
2021-08-09 20:01:30 +0530

    X00T: Update build fingerprint to August

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -113,8 +113,8 @@ void vendor_load_properties()
 
    // Safetynet Workaround
     property_override("ro.boot.verifiedbootstate", "green");
-    property_override("ro.build.description", "redfin-user 11 RQ3A.210705.001 7380771 release-keys");
-    property_override("ro.build.fingerprint", "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys");
-    property_override("ro.system.build.fingerprint", "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys");
+    property_override("ro.build.fingerprint", "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys");
+    property_override("ro.system.build.fingerprint", "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys");
 
 }
