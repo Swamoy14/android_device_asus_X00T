980c0b73ff2280c5eaf6cf74b4a1c3765392ec3b
Marc Bourgoin <themard85@gmail.com>
2021-09-14 13:04:04 +0530

    X00T: Drop QCOM WFD

--- device.mk
+++ device.mk
@@ -214,7 +214,6 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
-    libaacwrapper \
     libc2dcolorconvert \
     libmm-omxcore \
     libOmxAacEnc \
@@ -369,14 +368,6 @@ PRODUCT_PACKAGES += \
 # libstdc++: camera.sdm660
 PRODUCT_PACKAGES += \
     libstdc++.vendor
-
-# WFD
-PRODUCT_PACKAGES += \
-    libmediaextractorservice \
-    libnl
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
 
 # WiFi
 PRODUCT_PACKAGES += \
