a30a168b0a1beb3ce5c8af127d48e4aa896addd0
Albert I <kras@raphielgang.org>
2021-11-02 21:47:53 +0530

    X00T: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc

--- device.mk
+++ device.mk
@@ -153,6 +153,10 @@ PRODUCT_PACKAGES += \
 	qti_telephony_hidl_wrapper.xml \
 	qti-telephony-utils \
 	qti_telephony_utils.xml
+	
+# Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
 # IORAP
 PRODUCT_PACKAGES += \
