a77008c19407bfbec708bbd26ce627b94aefe685
Harshit Jain <harshitjain6751@gmail.com>
2021-11-02 21:41:02 +0530

    X00T: Strip off some debug packages
    * Ref: https://source.android.com/devices/tech/dalvik/configure#compilation_options
    
    Q: PRODUCT_MINIMIZE_JAVA_DEBUG_INFO wth does this do?
    A: https://android.googlesource.com/platform/build/+/master/target/product/go_defaults_common.mk#68
    
    Change-Id: Iad073ec85f5cfe5be5a8d1944dd7cff521077dbe

--- device.mk
+++ device.mk
@@ -352,6 +352,9 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 # Always preopt extracted APKs to prevent extracting out of the APK
 # for gms modules.
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+USE_DEX2OAT_DEBUG := false
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
