ec5d8d34fed72ee6501484258cc9d1050385f107
Swamoy14 <shubhasankar14@gmail.com>
2022-06-10 11:14:03 +0530

    X00T: Update build fingerprint to June

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -113,8 +113,8 @@ void vendor_load_properties()
 
    // Safetynet Workaround
     property_override("ro.boot.verifiedbootstate", "green");
-    property_override("ro.build.description", "raven-user 12 SP2A.220505.002 8353555 release-keys");
-    property_override("ro.build.fingerprint", "google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys");
-    property_override("ro.system.build.fingerprint", "google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys");
+    property_override("ro.build.description", "raven-user 12 SQ3A.220605.009.B1 8650216 release-keys");
+    property_override("ro.build.fingerprint", "google/raven/raven:12/SQ3A.220605.009.B1/8650216:user/release-keys");
+    property_override("ro.system.build.fingerprint", "google/raven/raven:12/SQ3A.220605.009.B1/8650216:user/release-keys");
 
 }
