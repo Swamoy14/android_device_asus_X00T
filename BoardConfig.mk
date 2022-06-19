b50b0ce9474a456b1a192f923d484e7060c8e330
Swamoy14 <shubhasankar14@gmail.com>
2022-06-19 10:07:03 +0530

    X00T: Remove unused sepolicy directory

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,12 +182,7 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 VENDOR_SECURITY_PATCH := 2020-08-05
 
 # SELinux
-#include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-#BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-
-# SELinux
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/minimal_sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
