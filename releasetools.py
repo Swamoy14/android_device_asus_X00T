3305e48465eec8443922608c45c0f4ef9617b43e
LuK1337 <priv.luk@gmail.com>
2022-10-21 20:43:18 +0530

    X00T: releasetools: Fix android-info.txt parsing
    This unbreaks OTA generation.
    
    Change-Id: I93466279b2786d9dc6265afcb0ea264707e92b05

--- releasetools.py
+++ releasetools.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2009 The Android Open Source Project
 # Copyright (c) 2011, The Linux Foundation. All rights reserved.
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,8 +28,8 @@ def IncrementalOTA_Assertions(info):
 
 def AddModemAssertion(info):
   android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
-  f = re.search(r'require\s+version-firmware\s*=\s*(.+)', android_info)
+  m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info.decode('utf-8'))
+  f = re.search(r'require\s+version-firmware\s*=\s*(.+)', android_info.decode('utf-8'))
   if m and f:
     version_modem = m.group(1).rstrip()
     version_firmware = f.group(1).rstrip()
