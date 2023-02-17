0fa3835e5319eb0cb2062cf45444dc2a88380892
kdrag0n <danny@kdrag0n.dev>
2023-02-17 22:46:42 +0530

    X00T: overlay: Compact cached app heaps in the background
    * Background ART heap compaction for cached apps has been recommended for Android Go devices for a while, and Google has enabled it for the Pixel 5 on RQ1A.201205.010.
    
    *This feature should help fit more cached apps in memory
    
    [iamimmanuelraj] Adapt it for rro
    
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>
    Signed-off-by: Ruchit <ruchitmarathe@gmail.com>
    Signed-off-by: Immanuel Raj <iamimmanuelraj@gmail.com>

--- device.mk
+++ device.mk
@@ -361,6 +361,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
+    
+# RRO Overlays
+PRODUCT_PACKAGES += \
+    SimpleDeviceConfigOverlay
 
 # Seccomp
 PRODUCT_COPY_FILES += \
